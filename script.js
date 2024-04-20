e017753da66967393f718e3c1249492d49966b9f
Omer <uckacomerkayra@gmail.com>
2024-04-20 11:24:06 -0500

    bleh

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const soundName = new Audio("boop.mp3");
+const music=new Audio(epic.mp3)
 window.addEventListener("load", playSound)
     boop.play();
 function playSound() {
     soundName.play()
+}
+function playMusic(){
+
 }
