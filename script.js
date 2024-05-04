909f92c2bac80e3a7419b9296fe1fdeccee9fdd5
Omer <uckacomerkayra@gmail.com>
2024-05-04 10:12:23 -0500

    mwahahaha

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const soundName = new Audio("boop.mp3");
 const music = new Audio("epic.mp3")
 const croak = new Audio("croak.mp3")
+const evil = new Audio("haha.mp3")
 window.addEventListener("load", playSound)
     boop.play();
 function playSound() {
@@ -11,3 +12,7 @@ music.play()
 }
 function playCroak(){
 croak.play()}
+
+function laffEvil(){
+    evil.play()
+}
